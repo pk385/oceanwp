11d6082b88e9c900ed464259b6c6c914fbf553fd
web-devise <manager@devise.com.ua>
2022-01-25 15:22:07 +0200

    Customizer Assets Loading on Front End Fix

--- inc/customizer/library/customizer-custom-controls/functions.php
+++ inc/customizer/library/customizer-custom-controls/functions.php
@@ -15,7 +15,6 @@ if ( ! function_exists( 'skyrocket_scripts_styles' ) ) {
 		wp_enqueue_style( 'fontawesome' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'skyrocket_scripts_styles' );
 add_action( 'customize_controls_print_styles', 'skyrocket_scripts_styles' );
 
 /**
